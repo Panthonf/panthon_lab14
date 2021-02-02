e13ff483262f64d6f59ac8a3dd1906686813ef88
Panthonf <76035497+Panthonf@users.noreply.github.com>
2021-02-02 17:19:57 +0700

    completed

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,28 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b,int &c,int &d){
+	int x[]={a,b,c,d};
+	int y[]={0,0,0,0};
+	int count=0;
+	
+	while(count<4){
+		bool state=false;
+		int ran =rand()%4;
+		for(int j=0;j<count;j++){
+			if(y[j]==ran){
+				state=true;
+				break;
+				
+			}
+		}
+		if(!state){
+			y[count]=ran;
+			count++;
+		}
+	}
+	a=x[y[0]];
+	b=x[y[1]];
+	c=x[y[2]];
+	d=x[y[3]];
+}
